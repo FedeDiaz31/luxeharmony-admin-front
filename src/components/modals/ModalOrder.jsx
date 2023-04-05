54ebca7b2fdac38c90881af1c96d973a75451455
magguer <martintelaguer@gmail.com>
2023-04-04 21:00:13 -0300

    modal-tablet-orders

---
+++ src/components/modals/ModalOrder.jsx
@@ -0,0 +1,106 @@
+/* CSS */
+import { useState } from "react";
+import "../../animation/Animations.css";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
+function ModalOrder({ handleCloseModalOrder, order }) {
+  const [idStatus, setIdStatus] = useState(order.status);
+  const user = useSelector((state) => state.user);
+  const [status, setStatus] = useState(null);
+  /*   Close with ESC Function */
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      handleCloseModalOrder();
+    }
+  });
+
+  useEffect(() => {
+    const getStatus = async () => {
+      const response = await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}/status`,
+      });
+      setStatus(response.data);
+    };
+    getStatus();
+  }, []);
+
+  /*  console.log(status); */
+
+  const handleEdit = async () => {
+    await axios({
+      headers: {
+        Authorization: `Bearer ${user.admin.token}`,
+      },
+      method: "patch",
+      url: `${process.env.REACT_APP_API_URL}/orders/${order._id}`,
+      data: { idStatus: idStatus },
+    });
+  };
+
+  return (
+    <div className="fixed inset-0 bg-modal z-40">
+      <div className="flex items-center justify-center min-h-screen text-center px-8 tablet:px-0">
+        <div
+          className="fixed inset-0 bg-[#0f0f0f7e] cursor-pointer fade-in-fast z-20"
+          onClick={handleCloseModalOrder}
+        ></div>
+
+        <div className="inline-block bg-bgPrimaryColor rounded-lg shadow-lg transform transition-all fade-in-fast duration-300 modal z-30 mt-12">
+          <button
+            className="absolute right-[-5px] top-[-5px] h-6 w-6 flex border justify-center border-bgSecondaryColor bg-bgPrimaryColor hover:bg-bgSecondaryColor rounded-full text-sm translate-all duration-150 font-bold hover:text-bgPrimaryColor"
+            onClick={handleCloseModalOrder}
+          >
+            X
+          </button>
+          {/*             Form Edit Product */}
+          <div className="flex text-white items-center p-5 px-10">
+            {/*                 Images Info of Product */}
+
+            <div className="flex flex-col">
+              <h2 className="font-bold text-lg">Order</h2>
+              <h3 className="text-lg font-light">ID: {order._id}</h3>
+              <div className="">
+                <div className="slide-top">
+                  {/*                     Page 2 Edit of Product */}
+                  <div className="mt-5">
+                    <label htmlFor="name">Status</label>
+                    <select
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="rounded bg-bgForthColor min-w-[150px] ml-10 mr-2 py-1 px-1"
+                      onChange={(e) => setIdStatus(e.target.value)}
+                    >
+                      {status?.map((st) => {
+                        return (
+                          <option key={st._id} value={st._id}>
+                            {st.name}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-5 flex flex-col  items-center">
+                <button
+                  onClick={handleEdit}
+                  className="mt-3 gap-2 flex items-center rounded p-2 pl-3 pr-4 hover:bg-bgSecondaryColor transition-all duration-200 hover:text-textPrimary"
+                >
+                  <img className="w-8" src="edit-icon.png" alt="" />
+                  <h2 className="font-bold">Edit</h2>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ModalOrder;
--- src/components/partials/OrderTableBody.jsx
+++ src/components/partials/OrderTableBody.jsx
@@ -1,40 +1,57 @@
 import { format } from "date-fns";
+import ModalOrder from "../modals/ModalOrder";
+import { useState } from "react";
 
 function OrderTableBody({ order }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModalOrder = () => setShowModal(false);
+  const handleShowModalOrder = () => setShowModal(true);
+
   return (
     <>
-      {order ? (
-        <li className="cursor-pointer flex items-center justify-between px-5 py-2 mx-2 rounded hover:scale-[101%] bg-bgPrimaryColor shadow transition-all duration-200">
-          <div className="w-full">
-            <h3 className="font-semibold">
-              {order.user.firstname} {order.user.lastname}
-            </h3>
+      <div className="z-20">
+        {showModal && (
+          <div>
+            <ModalOrder
+              order={order}
+              handleCloseModalOrder={handleCloseModalOrder}
+            />
           </div>
-          <div className="w-full text-end">
-            <h3 className="text-textTertiary hidden laptop:block">
-              {order.email}
-            </h3>
-          </div>
-          <div className="w-full text-center">
-            <h3 className="text-textTertiary">U$D {order.totalPrice}</h3>
-          </div>
-          <div className="w-full text-center hidden laptop:block">
-            <h3 className="text-textTertiary">
-              {format(new Date(order.createdAt), "dd'/'M'/'yy")}
-            </h3>
-          </div>
-          <div className="w-full text-center">
-            <h3 className="text-textTertiary">{order.status.name}</h3>
-          </div>
-          <div className="w-full text-end">
-            <button className=" hover:bg-bgPrimaryColor text-textPrimary px-3 py-1 rounded-b-md transition-all duration-200 font-bold">
-              <img className="w-6" src="edit-icon.png" alt="" />
-            </button>
-          </div>
-        </li>
-      ) : (
-        ""
-      )}
+        )}
+      </div>
+
+      <li
+        onClick={handleShowModalOrder}
+        className="cursor-pointer flex items-center justify-between px-5 py-2 mx-2 rounded hover:scale-[101%] bg-bgPrimaryColor shadow transition-all duration-200"
+      >
+        <div className="w-full">
+          <h3 className="font-semibold">
+            {order.user.firstname} {order.user.lastname}
+          </h3>
+        </div>
+        <div className="w-full text-end">
+          <h3 className="text-textTertiary hidden laptop:block">
+            {order.email}
+          </h3>
+        </div>
+        <div className="w-full text-center">
+          <h3 className="text-textTertiary">U$D {order.totalPrice}</h3>
+        </div>
+        <div className="w-full text-center hidden laptop:block">
+          <h3 className="text-textTertiary">
+            {format(new Date(order.createdAt), "dd'/'M'/'yy")}
+          </h3>
+        </div>
+        <div className="w-full text-center">
+          <h3 className="text-textTertiary">{order.status.name}</h3>
+        </div>
+        <div className="w-full text-end">
+          <button className=" hover:bg-bgPrimaryColor text-textPrimary px-3 py-1 rounded-b-md transition-all duration-200 font-bold">
+            <img className="w-6" src="edit-icon.png" alt="" />
+          </button>
+        </div>
+      </li>
     </>
   );
 }
