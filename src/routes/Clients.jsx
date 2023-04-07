c047d08808ad7403774dce79fa187db8ffa737cd
magguer <martintelaguer@gmail.com>
2023-04-07 12:34:14 -0300

    clients-fix

--- src/routes/Clients.jsx
+++ src/routes/Clients.jsx
@@ -86,15 +86,6 @@ function Users() {
 
         {clients ? (
           <>
-            {/* <div className="flex font-semibold text-lg px-5 mt-5">
-              <div className="w-full">Full name</div>
-              <div className="w-full text-end hidden laptop:block">Email</div>
-              <div className="w-full text-end">Orders</div>
-              <div className="w-full text-end hidden laptop:block">
-                Created at
-              </div>
-              <div className="w-full"></div>
-            </div> */}
             <ul className="mt-3 pb-4 grid gap-1">
               {searchClients.map((client, i) => {
                 return <ClientsTableBody key={i} client={client} />;
