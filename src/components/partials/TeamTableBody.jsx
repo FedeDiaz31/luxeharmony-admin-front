2ee893ecb486caa68fbc3af954a0632e5367972a
magguer <martintelaguer@gmail.com>
2023-04-07 13:30:10 -0300

    brands

--- src/components/partials/TeamTableBody.jsx
+++ src/components/partials/TeamTableBody.jsx
@@ -20,18 +20,18 @@ function ProductTableBody({ member }) {
       </div> */}
       <li
         onClick={handleCloseModalMember}
-        className="cursor-pointer flex items-center justify-between px-5 py-2 mx-2 rounded-lg  hover:scale-[101%] bg-bgPrimaryColor  shadow transition-all duration-200"
+        className="cursor-pointer flex items-center justify-between px-3 tablet:px-5 py-2 mx-2 rounded-lg  hover:scale-[101%] bg-bgPrimaryColor  shadow transition-all duration-200"
       >
         <div className="w-full">
           <h3 className="font-semibold">
             {member.firstname} {member.lastname}
           </h3>
         </div>
-        <div className="w-full text-center">
+        <div className="w-full text-end tablet:text-center">
           <h3 className="text-textTertiary text-sm">{member.rol}</h3>
         </div>
-        <div className="w-full text-end">
-          <h3 className="text-textTertiary text-sm">{member.email}</h3>
+        <div className="w-full hidden tablet:flex text-end">
+          <h3 className=" text-textTertiary text-sm">{member.email}</h3>
         </div>
         <div className="w-full text-end hidden laptop:block">
           <h3 className="text-textTertiary text-sm">
