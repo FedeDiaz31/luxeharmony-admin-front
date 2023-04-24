29c34da3146589521fa5feb1c8c3fb446a1c67a7
FedeDiaz31 <fede.diaz1231@gmail.com>
2023-04-24 14:30:01 -0300

    Discoment Firstname order table body

--- src/components/partials/OrderTableBody.jsx
+++ src/components/partials/OrderTableBody.jsx
@@ -25,7 +25,7 @@ function OrderTableBody({ order }) {
         className="cursor-pointer flex items-center justify-between px-3 tablet:px-5 py-2 mx-0 tablet:mx-2  rounded hover:scale-[101%] bg-bgPrimaryColor shadow transition-all duration-200"
       >
         <div className="w-full flex gap-1">
-          {/* <h3 className="font-semibold">{order.user.firstname}</h3> */}
+          <h3 className="font-semibold">{order.user.firstname}</h3>
           {/* <h3 className="hidden tablet:block font-semibold">
             {order.user.lastname}
           </h3> */}
